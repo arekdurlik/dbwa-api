6767a949224edfb7cd206ea85edd472f909dbaee
arekdurlik <72689308+arekdurlik@users.noreply.github.com>
2023-01-11 23:35:32 +0100

    fix library routing

--- routes/me.routes.ts
+++ routes/me.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { verifyToken } from '../middlewares/verify-token'
-import { getUserData, getEffectLibrary, getPedalboardLibrary, saveEffectToLibrary, removeEffectFromLibrary, savePedalboardToLibrary } from '../controllers/me.controller'
+import { getUserData, getEffectLibrary, getPedalboardLibrary, saveEffectToLibrary, removeEffectFromLibrary, savePedalboardToLibrary, removePedalboardFromLibrary } from '../controllers/me.controller'
 
 const router = Router()
 
@@ -9,7 +9,7 @@ router.get(   '/effects',         verifyToken, getEffectLibrary)
 router.post(  '/effects',         verifyToken, saveEffectToLibrary)
 router.delete('/effects/:id',     verifyToken, removeEffectFromLibrary)
 router.get(   '/pedalboards',     verifyToken, getPedalboardLibrary)
-router.delete('/pedalboards/:id', verifyToken, removeEffectFromLibrary)
+router.delete('/pedalboards/:id', verifyToken, removePedalboardFromLibrary)
 router.post(  '/pedalboards',     verifyToken, savePedalboardToLibrary)
 
 export default router
